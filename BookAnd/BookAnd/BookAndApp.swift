09e085803dbd38b473ec1eeea3478f8fea658fe8
delmaSong <sdy2856@gmail.com>
2025-06-06 15:08:16 +0900

    initial set

---
+++ BookAnd/BookAnd/BookAndApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAndApp.swift
+//  BookAnd
+//
+//  Created by Delma Song on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BookAndApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
