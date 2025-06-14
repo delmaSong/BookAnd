093515d028c679cda474c2fe930578798b4c2ebe
delmaSong <sdy2856@gmail.com>
2025-06-14 15:19:01 +0900

    ✨ 검색어 입력한 경우 목록 요청

--- BookAnd/BookAnd/ViewModel/BookSearchView.swift
+++ BookAnd/BookAnd/ViewModel/BookSearchView.swift
@@ -6,12 +6,39 @@
 //
 
 import SwiftUI
+import Combine
 
 struct BookSearchView: View {
+    @State private var inputText: String = ""
+    @StateObject private var viewModel = BookSearchViewModel()
     
     var body: some View {
         VStack(spacing: 20) {
             Spacer()
+            TextField("제목, 저자, 출판사 등을 입력하세요", text: $inputText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+            
+            Button {
+                if !inputText.isEmpty {
+                    viewModel.searchBooks(query: inputText)
+                }
+            } label: {
+                Text("검색")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+		.navigationTitle("도서 추가")
+		.navigationBarTitleDisplayMode(.inline)
+		.navigationBarBackButtonHidden(false)
+        .onChange(of: viewModel.books) { books in
         }
     }
 }
