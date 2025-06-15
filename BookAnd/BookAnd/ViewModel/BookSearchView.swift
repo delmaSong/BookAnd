00804a63d4f5d7ab30abc286f39f0f6715419832
delmaSong <sdy2856@gmail.com>
2025-06-15 17:31:28 +0900

    ✨검색 버튼 제거하고 키보드 리턴키 누르는 경우 검색 요청하도록 함

--- BookAnd/BookAnd/ViewModel/BookSearchView.swift
+++ BookAnd/BookAnd/ViewModel/BookSearchView.swift
@@ -14,27 +14,15 @@ struct BookSearchView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Spacer()
             TextField("제목, 저자, 출판사 등을 입력하세요", text: $inputText)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding(.horizontal)
-            
-            Button {
-                if !inputText.isEmpty {
-                    viewModel.searchBooks(query: inputText)
-                }
-            } label: {
-                Text("검색")
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
-            }
-            .padding(.horizontal)
-            
-            Spacer()
-        }
+                .onSubmit {
+                    if !inputText.isEmpty {
+                        viewModel.searchBooks(query: inputText)
+                    }
+				}
+		}
 		.navigationTitle("도서 추가")
 		.navigationBarTitleDisplayMode(.inline)
 		.navigationBarBackButtonHidden(false)
