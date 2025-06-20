9071812078f58e786c03f788a056f8e6f718f630
delmaSong <sdy2856@gmail.com>
2025-06-20 22:37:58 +0900

    ✨ 빈 BookView 선언

---
+++ BookAnd/BookAnd/ViewModel/BookView.swift
@@ -0,0 +1,18 @@
+//
+//  BookView.swift
+//  BookAnd
+//
+//  Created by Delma Song on 6/20/25.
+//
+
+import SwiftUI
+
+struct BookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookView()
+}
